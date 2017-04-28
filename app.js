b06c625b05cb45b8ab74447038eb44371cb47452
Michael Tambornino <michael.tambornino@gmail.com>
2017-04-28 11:05:56 -0500

    adding api calls

--- app.js
+++ app.js
@@ -1,1 +1,64 @@
 const express = require('express')
+const app = express()
+const port = process.env.PORT || 3030;
+const cors = require('cors')
+const rp = require('request-promise')
+
+
+// app.use((req, res, next) => {
+//   req.setHeader({'Access-Control-Allow-Origin': '*}');
+//   next();
+// });
+
+app.use(cors()) // allows cross origin sharing
+
+// / weekly projections by position
+app.get('/nerdprojections/:pos/:wk', function(req, res) {
+  rp({
+  uri: `https://www.fantasyfootballnerd.com/service/weekly-projections/json/iz33m4ducg4h/${req.params.pos}/${req.params.wk}`,
+  json: true
+})
+  .then((data) => {
+    res.json(data)
+  })
+})
+
+// weekly rankings by position
+app.get('/nerdrankings/:pos/:wk', function(req, res) {
+  rp({
+  uri: `https://www.fantasyfootballnerd.com/service/weekly-rankings/json/iz33m4ducg4h/${req.params.pos}/${req.params.wk}/1/`,
+  json: true
+})
+  .then((data) => {
+    res.json(data)
+  })
+})
+
+// nfl weekly stats
+app.get('/weekstats/:wk', function(req, res) {
+  rp({
+  uri: `http://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&season=2016&week=${req.params.wk}&format=json`,
+  json: true
+})
+  .then((data) => {
+    res.json(data)
+  })
+})
+
+// espn expert rankings
+app.get('/espnexperts/:wk/:pos/:exp', function(req, res) {
+  rp({
+  uri: `http://api.fantasy.nfl.com/v1/players/editorweekranks?season=2016&week=${req.params.wk}&position=${req.params.pos}&format=json&editorId=${req.params.exp}&count=100`,
+  json: true
+})
+  .then((data) => {
+    res.json(data)
+  })
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
