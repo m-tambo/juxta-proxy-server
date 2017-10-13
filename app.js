82484c9dfa5b234286afcae7ddb2647e75789db4
Michael Tambornino <michael.tambornino@gmail.com>
2017-10-12 19:06:41 -0500

    Adjust app for 2017 season

--- app.js
+++ app.js
@@ -3,68 +3,66 @@ const app = express();
 const port = process.env.PORT || 4040;
 const cors = require('cors');
 const rp = require('request-promise');
-const keepServerAwake = require('./keepServerAwake.js');
 require('dotenv').config();
 
 const nerdAPI = process.env.NERD_API_KEY;
 
-// keepServerAwake();
+  // ALLOW CROSS-ORIGIN SHARING
+app.use(cors())
 
-app.use(cors()) // allows cross origin sharing
-
-// / weekly projections by position
+  // WEEKLY PROJECTIONS BY POSITION
 app.get('/nerdprojections/:pos/:wk', function(req, res) {
   rp({
-  uri: `https://www.fantasyfootballnerd.com/service/weekly-projections/json/${nerdAPI}/${req.params.pos}/${req.params.wk}`,
-  json: true
-})
-  .then((data) => {
-    res.json(data)
+    uri: `https://www.fantasyfootballnerd.com/service/weekly-projections/json/${nerdAPI}/${req.params.pos}/${req.params.wk}`,
+    json: true
   })
+    .then((data) => {
+      res.json(data)
+    })
 })
 
-// weekly rankings by position
+  // WEEKLY RANKINGS BY POSITION
 app.get('/nerdrankings/:pos/:wk', function(req, res) {
   rp({
-  uri: `https://www.fantasyfootballnerd.com/service/weekly-rankings/json/${nerdAPI}/${req.params.pos}/${req.params.wk}/1/`,
-  json: true
-})
-  .then((data) => {
-    res.json(data)
+    uri: `https://www.fantasyfootballnerd.com/service/weekly-rankings/json/${nerdAPI}/${req.params.pos}/${req.params.wk}/1/`,
+    json: true
   })
+    .then((data) => {
+      res.json(data)
+    })
 })
 
-// nfl weekly stats
+  // NFL WEEKLY STATS
 app.get('/weekstats/:wk', function(req, res) {
   rp({
-  uri: `http://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&season=2016&week=${req.params.wk}&format=json`,
-  json: true
-})
-  .then((data) => {
-    res.json(data)
+    uri: `http://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&season=2017&week=${req.params.wk}&format=json`,
+    json: true
   })
+    .then((data) => {
+      res.json(data)
+    })
 })
 
-// espn expert rankings
+  // ESPN EXPERT RANKINGS
 app.get('/espnexperts/:wk/:pos/:exp', function(req, res) {
   rp({
-  uri: `http://api.fantasy.nfl.com/v1/players/editorweekranks?season=2016&week=${req.params.wk}&position=${req.params.pos}&format=json&editorId=${req.params.exp}&count=100`,
-  json: true
-})
-  .then((data) => {
-    res.json(data)
+    uri: `http://api.fantasy.nfl.com/v1/players/editorweekranks?season=2017&week=${req.params.wk}&position=${req.params.pos}&format=json&editorId=${req.params.exp}&count=100`,
+    json: true
   })
+    .then((data) => {
+      res.json(data)
+    })
 })
 
-// espn player news
+  // ESPN PLAYER NEWS
 app.get('/playernews', function(req, res) {
   rp({
-  uri: `http://api.fantasy.nfl.com/v1/players/news?format=json&count=200`,
-  json: true
-})
-  .then((data) => {
-    res.json(data)
-  })
+      uri: `http://api.fantasy.nfl.com/v1/players/news?format=json&count=200`,
+      json: true
+    })
+    .then((data) => {
+      res.json(data)
+    })
 })
 
 app.get('/', function(req, res){
